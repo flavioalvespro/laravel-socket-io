46217e28f670aedc8de0e533d7b50afaabd48fd8
Flavio Alves <flavioalvespro@gmail.com>
2021-04-06 15:54:16 -0300

    creating users and posts for examples

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/create-post', function(){
+    $user = User::first();
+
+    $user->posts()->create([
+        'title' => Str::random(150),
+        'body' => Str::random(400),
+    ]);
+
+    return 'ok';
+});
 
 Route::get('/', function () {
     return view('welcome');
